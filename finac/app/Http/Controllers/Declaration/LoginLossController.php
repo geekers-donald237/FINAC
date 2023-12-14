4972d9adf52267bb6a43898263b4bd88daf22d36
Kewou Mbotchak Christian Durand <92410456+DurandKewou@users.noreply.github.com>
2023-12-14 22:00:53 +0100

    Update LoginLossController.php

--- finac/app/Http/Controllers/Declaration/LoginLossController.php
+++ finac/app/Http/Controllers/Declaration/LoginLossController.php
@@ -16,49 +16,19 @@ class LoginLossController extends Controller
 
     public function store(Request $request)
     {
-        // Vérifier les informations d'identification
-        $credentials = $request->only('code_finac', 'numero_serie');
-
-        // Vérifier le code_finac dans la table permissionsPort
-        $permissionsPort = PermissionsPort::where('code_finac', $credentials['code_finac'])->first();
-
-
-D'accord, voici le contrôleur modifié pour effectuer la vérification du code_finac dans la table permissionsPort et du numero_serie dans la table Weapons en utilisant la relation avec la table permissionsPort où id_weapons est présent :
-
-php
-Copy code
-<?php
-
-namespace App\Http\Controllers\Declaration;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\Helpers\HelpersFunction;
-use App\Models\PermissionsPort;
-
-class LoginLossController extends Controller
-{
-    public function showLoginForm()
-    {
-        return view('welcome');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
+         $request->validate([
             'code_finac' => 'required',
-            'numero_serie' => 'required',
+            'serial_number' => 'required',
         ]);
-
         // Vérifier les informations d'identification
-        $credentials = $request->only('code_finac', 'numero_serie');
+        $credentials = $request->only('code_finac', 'serial_number');
 
         // Vérifier le code_finac dans la table permissionsPort
         $permissionsPort = PermissionsPort::where('code_finac', $credentials['code_finac'])->first();
 
         if ($permissionsPort && $permissionsPort->weapons) {
             // Vérifier que le numero_serie existe pour cet id_weapons dans la table Weapons
-            $weapons = $permissionsPort->weapons->where('numero_serie', $credentials['numero_serie'])->first();
+            $weapons = $permissionsPort->weapons->where('serial_number', $credentials['serial_number'])->first();
 
             if ($weapons) {
                 // Informations correctes, rediriger vers la page LossDeclaration
